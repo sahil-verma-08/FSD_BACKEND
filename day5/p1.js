f192ae916d11b834e4e014cef6a51b7ef1396408
sahil-verma-08 <sahilverma2431@gmail.com>
2025-02-25 12:17:40 +0530

    25 feb

---
+++ day5/p1.js
@@ -0,0 +1,8 @@
+const express =require('express');
+const app=express();
+app.get("/",(req,res)=>{
+    res.send("hello");
+});
+app.listen(9000,()=>{
+    console.log('server is running on 9000');
+});
